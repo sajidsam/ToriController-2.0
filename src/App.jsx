6d35460ace029c4fc3e79e4d42b10a1131e3bfa4
sifat-99 <mdabdurrahmansifat@gmail.com>
2026-07-20 13:10:38 +0600

    style(ios): adapt viewport and touch interactions for iPad

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import SubmarineDashboard from './components/SubmarineDashboard'
 
 function App() {
   return (
-    <div className="bg-black font-sans text-white h-screen w-screen overflow-hidden flex flex-col selection:bg-white selection:text-black">
+    <div className="bg-black font-sans text-white h-screen w-screen overflow-hidden flex flex-col selection:bg-white selection:text-black pt-[env(safe-area-inset-top)] pb-[env(safe-area-inset-bottom)] pl-[env(safe-area-inset-left)] pr-[env(safe-area-inset-right)]">
       <TitleBar/>
       <SubmarineDashboard/>
     </div>
